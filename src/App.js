55ff3a5f3137ebf0a43eee0a24dc57ca7dbc7753
Sarah Ing <sarahikeda@gmail.com>
2017-10-06 15:13:58 -0400

    Beginning of boilerplate using freecodecamp resource

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React/Electron</h2>
+        </div>
+        <p className="App-intro">
+          Hello Electron!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
