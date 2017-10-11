295c2c45bd127dd3695ef3e143b7360bcc676575
Sarah Ing <sarahikeda@gmail.com>
2017-10-11 15:24:16 -0400

    Add component for welcome text once logged in

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 export class Welcome extends React.Component {
   render() {
     return (
-      <div>
-        <h3>Welcome to the FirstNet App Review Utility. Please sign in to get started.</h3>
+      <div className="login">
+        <div className="welcome-info">
+          <img className='first-net-logo' alt="first-net-logo" src={require('../../../images/logo.png')} />
+          <div className="welcome-text">
+            <h3>Welcome Sean to the FirstNet App Review Utility. </h3>
+            <p>To review an Application simply type the application name in the field above (Find Application) or find all applications by status by selecting from top right menu.</p>
+            <p>Below are all apps with a status of UNDER REVIEW...</p>
+          </div>
+        </div>
       </div>
     );
   }
