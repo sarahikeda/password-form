983c2a3e64c2db21cd35efc8e7715ac88657f9d5
Sarah Ing <sarahikeda@gmail.com>
2017-10-11 15:25:25 -0400

    Add table for review of apps and add styling

---
+++ src/components/AppReview/AppReview.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+export class AppReview extends React.Component {
+  render() {
+    return (
+      <div className="app-review">
+        <Table>
+          <tbody>
+            <tr>
+              <td><img className='app-img' alt="screenshot of app" src={require('../../../images/app-img.png')} /></td>
+              <td>
+                <p className="app-name">Etiam aliquam pharetra</p>
+                <p>Version: 1.0.0</p>
+              </td>
+              <td>Platform: Android</td>
+              <td>
+                <p>Status: Under Review</p>
+                <p className="app-created-date">Submitted Date: September 18, 2017|11:25 AM</p>
+              </td>
+            </tr>
+            <tr>
+              <td><img className='app-img' alt="screenshot of app" src={require('../../../images/app-img.png')} /></td>
+              <td>
+                <p className="app-name">Etiam aliquam pharetra</p>
+                <p>Version: 1.0.0</p>
+              </td>
+              <td>Platform: Android</td>
+              <td>
+                <p>Status: Under Review</p>
+                <p className="app-created-date">Submitted Date: September 18, 2017|11:25 AM</p>
+              </td>
+            </tr>
+            <tr>
+              <td><img className='app-img' alt="screenshot of app" src={require('../../../images/app-img.png')} /></td>
+              <td>
+                <p className="app-name">Etiam aliquam pharetra</p>
+                <p>Version: 1.0.0</p>
+              </td>
+              <td>Platform: Android</td>
+              <td>
+                <p>Status: Under Review</p>
+                <p className="app-created-date">Submitted Date: September 18, 2017|11:25 AM</p>
+              </td>
+            </tr>
+            <tr>
+              <td><img className='app-img' alt="screenshot of app" src={require('../../../images/app-img.png')} /></td>
+              <td>
+                <p className="app-name">Etiam aliquam pharetra</p>
+                <p>Version: 1.0.0</p>
+              </td>
+              <td>Platform: Android</td>
+              <td>
+                <p>Status: Under Review</p>
+                <p className="app-created-date">Submitted Date: September 18, 2017|11:25 AM</p>
+              </td>
+            </tr>
+            <tr>
+              <td><img className='app-img' alt="screenshot of app" src={require('../../../images/app-img.png')} /></td>
+              <td>
+                <p className="app-name">Etiam aliquam pharetra</p>
+                <p>Version: 1.0.0</p>
+              </td>
+              <td>Platform: Android</td>
+              <td>
+                <p>Status: Under Review</p>
+                <p className="app-created-date">Submitted Date: September 18, 2017|11:25 AM</p>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
