432950a618acb1436cf3ab82d666ba59995ee0fa
Sarah Ing <sarahikeda@gmail.com>
2017-10-11 10:35:49 -0400

    Add separate component for Welcome text and style login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Welcome } from '../Welcome/Welcome';
+
 
 export class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       id: '',
-      password: ''
+      password: '',
+      isLoggedIn: false
     };
     this.handleIdChange = this.handleIdChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -22,25 +25,26 @@ export class Login extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state)
+    this.setState({isLoggedIn: true})
     // go to services and login user with S3 Credentials
+    console.log(this.state)
   }
 
   render() {
     return (
-      <div className="login">
+      <div className="landing">
         <div className="welcome-info">
           <img className='first-net-logo' alt="first-net-logo" src={require('../../../images/logo.png')} />
           <div className="welcome-text">
-            <h3>Welcome to the FirstNet App Review Utility. Please sign in to get started.</h3>
+            <Welcome />
           </div>
         </div>
-        <form className="user-info" onSubmit={this.handleSubmit}>
-          <label><span>User ID</span></label>
+        <form className="login" onSubmit={this.handleSubmit}>
+          <label>User ID</label>
           <input type='text' className="user-fields" name="id" value={this.state.id} onChange={this.handleIdChange}/>
-          <label><span>Password</span></label>
-            <input type='text'className="user-fields"  name="password" value={this.state.password} onChange={this.handlePasswordChange} />
-          <input className="submit-button" type="submit" value="Sign In" />
+          <label className="user-fields"><span>Password</span></label>
+          <input type='text' name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+          <button type="submit" className="submit-form">Sign In</button>
         </form>
       </div>
     );
