439db07d04e5d430a6361e00e77e2c74f9b8b386
Sarah Ing <sarahikeda@gmail.com>
2017-10-12 17:05:17 -0400

    Add Detailed Info component and styling

---
+++ src/components/AppDetailedInfo/AppDetailedInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
+import { ReviewDescription } from '../ReviewDescription/ReviewDescription';
+
+export class AppDetailedInfo extends React.Component {
+  render() {
+    return (
+      <div className='app-detailed-info'>
+        <Tabs defaultActiveKey={1}>
+          <Tab eventKey={1} title="REVIEW"><ReviewDescription/></Tab>
+          <Tab eventKey={2} title="DOWNLOAD ASSETS">DOWNLOAD ASSETS content</Tab>
+          <Tab eventKey={3} title="APPROVE/REJECT">APPROVE/REJECT content</Tab>
+        </Tabs>
+      </div>
+    )
+  }
+};
