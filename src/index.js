4c366846b14e0bcd91a4015a050fa4cba546d111
Elsa Wang <elsawang1230@gmail.com>
2017-10-13 14:50:13 -0400

    move scss out of src folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-
-import './styles/css/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
 import {appStore} from "./core/stores/app.store";
-import "../styles/App/App.css";
+import "../styles/index.css";
 
 
 ReactDOM.render(
