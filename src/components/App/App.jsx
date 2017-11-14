40b44da9e02b037604bf00c0eb2424cfadde0ab8
Sarah Ing <sarahikeda@gmail.com>
2017-11-14 14:59:08 -0500

    Put password rules in parent component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,23 @@ import Header from '../Header/Header';
 import PasswordForm from '../Password/PasswordForm';
 
 class App extends React.Component {
-
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passwordConditions: [{rule: /.{7,}/, description: '7 characters minimum'}, {rule: /[a-z]/, description: 'Lowercase letter'}, {rule: /[A-Z]/, description: 'Capital letter'}, {rule: /\d+/, description: 'Number'}, {rule: /[-\/\\^$*+?.()!|[\]{}#@%&]/, description: 'Special character'}]
+    }
+  }
+  renderApp() {
     return (
       <main className='container'>
         <Header/>
-        <PasswordForm/>
+        <PasswordForm {...this.state}/>
       </main>
     );
+  }
+
+  render() {
+    return this.renderApp();
   }
 }
 
