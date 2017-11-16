5c9a975bc8600bb4fd354a789d861fc5c149c9a4
Sarah Ing <sarahikeda@gmail.com>
2017-11-16 11:19:54 -0500

    Style header and add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
 
-const Header = () => {
-  return (
-    <div className="app-header">
-      <p>Private Bank</p>
-      <p>SELECT</p>
-    </div>
-  )
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="app-header">
+        <Navbar className="navigation-bar">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <img
+                className="goldman-logo"
+                src={require("../../../images/goldman-logo.png")}
+                alt="goldman-logo"/>
+            </Navbar.Brand>
+            <Navbar.Text className="sub-header">
+              <div className="description-text">Private Bank</div>
+              <div className="sub-description-text">SELECT</div>
+            </Navbar.Text>
+           </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <Navbar.Text>
+              <div className="nav-links">Help</div>
+            </Navbar.Text>
+            <Navbar.Text>
+              <div className="nav-links">Logout</div>
+            </Navbar.Text>
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )
+  }
 }
 
 export default Header;
