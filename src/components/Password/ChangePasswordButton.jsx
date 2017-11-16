6219b57c7771345dc004e7be91333e8328fdfe8e
Sarah Ing <sarahikeda@gmail.com>
2017-11-16 12:18:23 -0500

    Change button to component rather than constant

--- src/components/Password/ChangePasswordButton.jsx
+++ src/components/Password/ChangePasswordButton.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const ChangePasswordButton = () => {
-  return (
-    <Button
-      className="btn-primary change-password-button"
-      bsSize="large">Change Password
-    </Button>
-  )
+class ChangePasswordButton extends React.Component {
+  renderChangePasswordButton() {
+    return (
+      <Button
+        className="btn-primary change-password-button"
+        bsSize="large">Change Password
+      </Button>
+    );
+  }
+
+  render() {
+    return this.renderChangePasswordButton();
+  }
 }
 
 export default ChangePasswordButton;
